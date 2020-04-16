ffef19854c478d1d89992a8b2c68ae2d2295e3d2
Leonor Colbert <leonoracolbert@gmail.com>
2020-04-16 17:25:12 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Leonor made this, and then she changed it."
   end
 
 end
